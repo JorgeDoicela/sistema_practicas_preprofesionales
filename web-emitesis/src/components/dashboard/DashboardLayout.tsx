d0eda8e455b99285079f8beff87964717284539a
JorgeDoicela <ismael02doicela@gmail.com>
2026-05-17 22:47:04 -0500

    k

--- web-emitesis/src/components/dashboard/DashboardLayout.tsx
+++ web-emitesis/src/components/dashboard/DashboardLayout.tsx
@@ -255,7 +255,7 @@ export function DashboardLayout({ children }: { children: React.ReactNode }) {
             <motion.div
               initial={{ opacity: 0, y: 10 }}
               animate={{ opacity: 1, y: 0 }}
-              className="max-w-7xl mx-auto pb-20"
+              className="max-w-[1600px] mx-auto pb-20"
             >
               {children}
             </motion.div>
